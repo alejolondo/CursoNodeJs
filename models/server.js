37206e4a7cf445c18ab75d6a3bd8980cc9d67a9e
Alejandro Londoño <alejandro@pricezing.co>
2024-04-10 18:31:03 -0500

    Convertir respuesta a json en CRUD

--- models/server.js
+++ models/server.js
@@ -24,19 +24,27 @@ class Server {
 
     routes(){
         this.app.get('/api', (req, res) => {
-            res.send('Hello World, GET')
+            res.json({
+                msg: 'Hello World, GET'
+            })
           })
 
           this.app.post('/api', (req, res) => {
-            res.send('Hello World, POST')
+            res.json({
+                msg: 'Hello World, POST'
+            })
           })
 
           this.app.put('/api', (req, res) => {
-            res.send('Hello World, PUT')
+            res.json({
+                msg: 'Hello World, PUT'
+            })
           })
 
           this.app.delete('/api', (req, res) => {
-            res.send('Hello World, DELETE')
+            res.json({
+                msg: 'Hello World, DELETE'
+            })
           })
     }
 
